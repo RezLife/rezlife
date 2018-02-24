819041d838073af3eca631b74edd552b9463fa21
TimKartawijaya <tim.kartawijaya@gmail.com>
2018-02-23 23:03:59 -0600

    added Home button to emergency

--- public/js/emergency-control.js
+++ public/js/emergency-control.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('#tornado-button').click(function(){
         $('#emergency-content').load("/views/emergencies/tornado.html");
     });
+
+    $('#emergency-home-button').click(function(){
+        $('#emergency-content').load("/views/emergencies/emergency-home.html");
+    });
+    
 });
