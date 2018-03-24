6258260d8494cd40550b17967d492f1a9721f1c0
SubjectT <tristan.hoppe@my.wheaton.edu>
2018-03-24 17:02:37 -0500

    Trying to make the date work

--- app.js
+++ app.js
@@ -92,17 +92,9 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/homepage.html'));
 });
 
-<<<<<<< HEAD
-=======
-app.get('/demo', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/demo.html'));
-});
-
 app.post('/demo', (req, res) => {
     res.send("you posted! Nice.");
 });
-
->>>>>>> 53c8d6c6bc2107e7b65824dadaaddb4f5e381d65
 app.get('/login', function (req, res) {
     req.session.user = null;
     res.sendFile(path.join(__dirname, 'views/login.html'));
