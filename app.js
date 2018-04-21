dfba00c30bf1c897132ebd57aae93628e334c52d
malaikagilboy <malaika.gilboy@my.wheaton.edu>
2018-04-21 16:59:31 -0500

    Moved forgot password code out of app.js

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var fs = require('fs');
 var mysql = require('mysql');
 var fileUpload = require('express-fileupload');
 var chartParser = require('./chartParser.js');
-var createAccount = require('./createAccount.js');
-var sendEmail = require('./sendEmail.js');
+var createAccount = require('./controller/createAccount.js');
+var sendEmail = require('./controller/sendEmail.js');
+var login = require('./controller/login.js');
 var session = require('client-sessions');
 var bcrypt = require('bcrypt');
 const saltRounds = 11; //number of salt rounds for encryption
@@ -173,42 +174,7 @@ app.use('/resapp', resapp);
 //post method called after a user enters their email address to change their password
 app.post('/login/forgot', function (req, res) {
     if (req.body && req.body.email) {
-        var email = req.body.email;
-        var password = generator.generate();
-
-        //verify that the email is for a valid account
-        var sql = `SELECT * FROM t_users WHERE email = '${email}'`;
-        con.query(sql, function (err, result) {
-            if (err) {
-                console.log(err);
-                return res.status(400).send(err);
-            } else if (result.length > 0) {
-                //send email with the temporary password
-                sendEmail.emailPassword(email, password);
-
-                //encrypt the password
-                bcrypt.hash(password, saltRounds, function (err, hash) {
-                    if (err) {
-                        console.log("Error hashing password: " + err);
-                    } else {
-                        //update user password
-                        var sql = `UPDATE t_users SET password = '${hash}' WHERE email = '${email}'`;
-                        con.query(sql, function (err, result) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                console.log("1 record updated");
-                            }
-                        });
-                    }
-                });
-                res.redirect("/login");
-            }
-            else {
-                console.log("No user found: ", result);
-                return res.status(400).send('No user found with that email.');
-            }
-        });
+        login.forgotPass(req, res, con);
     } else {
         return res.status(400).send('Must enter email.');
     }
