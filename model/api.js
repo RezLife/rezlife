8ede2beb16c2c7d1a0b79241cb0248259892a2b7
asherbernardi <asher.bernardi@my.wheaton.edu>
2018-04-21 14:53:13 -0500

    for merge

--- model/api.js
+++ model/api.js
@@ -68,10 +68,10 @@ exports.searchAllStudents = (req,res,query,order) => {
     // all spaces are replaced with ORs in the regex
     var oredWords = query.replace(/ /g, '|');
     var searchString = '.*(' + oredWords + ').*';
-    con.query('SELECT * FROM t_students WHERE building RLIKE ? OR floor_and_room RLIKE ? OR name_first RLIKE ?'+
+    con.query('SELECT * FROM t_students WHERE building RLIKE ? OR floor RLIKE ? OR room RLIKE ? OR name_first RLIKE ?'+
             'OR name_last RLIKE ? OR studentID RLIKE ? OR name_preferred RLIKE ? OR city RLIKE ? OR state_province RLIKE ?'+
             'ORDER BY '+order.split(';')[0],
-            [searchString,searchString,searchString,searchString,searchString,searchString, searchString, searchString], (error, results, fields) => {
+            [searchString,searchString,searchString,searchString,searchString,searchString, searchString, searchString, searchString], (error, results, fields) => {
         if (error) return res.status(500).send(error); //need work
         return res.status(200).json({ results });
     });
