8a247dec9b7f1c880da90225d2f307bb37933949
malaikagilboy <malaika.gilboy@my.wheaton.edu>
2018-04-21 17:00:41 -0500

    Added separate folder for controller js

---
+++ controller/sendEmail.js
@@ -0,0 +1,62 @@
+//modules
+var nodemailer = require('nodemailer');
+
+//email account settings
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'noreplyrezlife@gmail.com',
+        pass: 'eK2BieN83'
+    }
+});
+
+//email feedback
+var feedbackTran = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'resappteam@gmail.com',
+        pass: 'eK2BieN83'
+    }
+});
+
+exports.emailFeedback = function (email, feedback) {
+    //layout for the email
+    var mailOptions = {
+        from: 'resappteam@gmail.com',
+        to: 'resappteam@gmail.com',
+        subject: 'Feedback for WebApp',
+        text: 'User who sent the feedback: ' + email + 
+        '. Their comments:' + 
+        feedback
+    };
+
+    //send the email with temporary password
+    feedbackTran.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+};
+
+exports.emailPassword = function (email, password) {
+    //layout for the email
+    var mailOptions = {
+        from: 'noreplyrezlife@gmail.com',
+        to: email,
+        subject: 'Resident Life App Password',
+        text: 'This is your temporary password: ' + password + 
+        '. Go to the Settings tab to update your password after logging in.' + 
+        '- Rezlife App Team'
+    };
+
+    //send the email with temporary password
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+};
