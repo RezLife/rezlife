124e8defe05883cdfebb2ba220243c0885bbd9a1
asherbernardi <asher.bernardi@my.wheaton.edu>
2018-04-23 17:03:23 -0500

    a bunch of changes to Accounts (sorry Samid). Now the error handling is the same as the rest of the site (alerts), and I've been testing the changes and not found any errors. It should work as before.

--- public/js/accounts.js
+++ public/js/accounts.js
@@ -3,17 +3,15 @@
 * Handles account page input/rendering.
 */
 
-
-
 //Show dorms for RA Account creation
 $('#roles').change(function () {
     //If Admin or Select hide the dorm and floor options
-    if ($(this).val() === "Admin"|| $(this).val() == "Select") {
+    if ($(this).val() === "Admin" || $(this).val() == "Select") {
         $('#dorms').addClass("hidden");
         $('#floors').addClass("hidden");
        // $('#fiscFL').hide();
     }
-    //If RA shoow list of dorms
+    //If RA show list of dorms
     if ($(this).val() === "RA") {
         $('#dorms').removeClass("hidden");
     }
@@ -22,4 +20,73 @@ $('#roles').change(function () {
 //Show floors for RA Account creation
 $('#dorms').change(function () {
      $('#floors').removeClass("hidden");
+});
+
+//Use Ajax for the post method so we can get better error handling
+$("#create-account").submit(function (event) {
+
+    //stop submit the form, we will post it manually.
+    event.preventDefault();
+
+    // Get form
+    var form = $('#create-account')[0];
+
+    // Create an FormData object
+    var data = new FormData(form);
+    // disabled the submit button
+    $("#create-submit").prop("disabled", true);
+
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: "/accounts",
+        data: data,
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data, status, xhr) {
+            alert("SUCCESS : " + xhr.responseText);
+            $("#create-submit").prop("disabled", false);
+        },
+        error: function (xhr) {
+            alert("ERROR : " + xhr.responseText);
+            $("#create-submit").prop("disabled", false);
+        }
+    });
+});
+
+//Use Ajax for the post method so we can get better error handling
+$("#delete-account").submit(function (event) {
+    if (confirm('Are you sure you would like to delete this account?\nThis action cannot be undone.')) {
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        // Get form
+        var form = $('#delete-account')[0];
+
+        // Create an FormData object
+        var data = new FormData(form);
+        // disabled the submit button
+        $("#delete-submit").prop("disabled", true);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "/deleteAccount",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (data, status, xhr) {
+                alert("SUCCESS : " + xhr.responseText);
+                $("#delete-submit").prop("disabled", false);
+            },
+            error: function (xhr) {
+                alert("ERROR : " + xhr.responseText);
+                $("#delete-submit").prop("disabled", false);
+            }
+        });
+    }
 });
